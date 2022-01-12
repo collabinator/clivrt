a3816d78146d458eb190161b30481208f3787674
Andy Krohg <akrohg@redhat.com>
2022-01-11 21:18:03 -0500

    Print console clear  before streaming instead of between frames, and now we can use color :)

--- media/ascii.py
+++ media/ascii.py
@@ -1,5 +1,9 @@
 from video_to_ascii import video_engine
 from aiortc.contrib.media import MediaStreamTrack
+import sys
+PLATFORM = 0
+if sys.platform != 'win32':
+    PLATFORM = 1
 
 class VideoTransformTrack(MediaStreamTrack):
     """
@@ -11,11 +15,15 @@ class VideoTransformTrack(MediaStreamTrack):
     def __init__(self, track):
         super().__init__()
         self.track = track
+        if PLATFORM:
+            sys.stdout.write("echo -en '\033[2J' \n")
+        else:
+            sys.stdout.write('\033[2J')
 
     async def recv(self):
         frame = await self.track.recv()
 
-        ve = video_engine.VideoEngine("just-ascii")
+        ve = video_engine.VideoEngine()
         ve.render_strategy.render_frame(frame.to_ndarray(format="bgr24"))
         
         return frame
